76af1094a8481bff1988624e3f67122e45404f3d
raru <raru@mulisha.ec>
2022-09-22 16:29:14 -0300

    changes studio

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -5,8 +5,8 @@ import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 // document schemas
-// import author from './documents/author'
-// import category from './documents/category'
+import author from './documents/author'
+import category from './documents/category'
 import post from './documents/post'
 import siteSettings from './documents/siteSettings'
 
@@ -31,7 +31,7 @@ export default createSchema({
     category,
     author,
     mainImage,
-    // authorReference,
+    authorReference,
     bodyPortableText,
     bioPortableText,
     excerptPortableText
