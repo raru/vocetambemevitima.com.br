ab0904391685e38eeb744c583ca80aaddb02fca0
raru <raru@mulisha.ec>
2022-09-22 18:05:59 -0300

    instagram

--- web/src/pages/Vozes.vue
+++ web/src/pages/Vozes.vue
@@ -8,14 +8,14 @@ Página com galeria de vídeos e fotos das ações realizadas até o momento.
 (Os conteúdos estarão principalmente no instagram, se houver uma maneira de embedar de lá, vamos precisar. Caso não dê podemos checar a possibilidade de subir no youtube)
 
 <!-- SnapWidget -->
-<script src="https://snapwidget.com/js/snapwidget.js"></script>
+
 <iframe src="https://snapwidget.com/embed/1011204" class="snapwidget-widget" allowtransparency="true" frameborder="0" scrolling="no" style="border:none; overflow:hidden;  width:100%; "></iframe>
 
 
     </Layout>
 </template>
 
-
+<script src="https://snapwidget.com/js/snapwidget.js"></script>
 <script>
     
     export default {
