88f00bf3222721f96f31f7975133d748476dd163
raru <raru@mulisha.ec>
2022-09-22 16:56:50 -0300

    footer e schema.js

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -15,7 +15,7 @@ import bodyPortableText from './objects/bodyPortableText'
 import bioPortableText from './objects/bioPortableText'
 import excerptPortableText from './objects/excerptPortableText'
 import mainImage from './objects/mainImage'
-// import authorReference from './objects/authorReference'
+import authorReference from './objects/authorReference'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
