df8bed53a066f2b8268e24689618a2d6db57c34a
raru <raru@mulisha.ec>
2022-09-22 16:02:46 -0300

    changed StUDIO

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -5,8 +5,8 @@ import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 // document schemas
-import author from './documents/author'
-import category from './documents/category'
+// import author from './documents/author'
+// import category from './documents/category'
 import post from './documents/post'
 import siteSettings from './documents/siteSettings'
 
@@ -15,7 +15,7 @@ import bodyPortableText from './objects/bodyPortableText'
 import bioPortableText from './objects/bioPortableText'
 import excerptPortableText from './objects/excerptPortableText'
 import mainImage from './objects/mainImage'
-import authorReference from './objects/authorReference'
+// import authorReference from './objects/authorReference'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -31,7 +31,7 @@ export default createSchema({
     category,
     author,
     mainImage,
-    authorReference,
+    // authorReference,
     bodyPortableText,
     bioPortableText,
     excerptPortableText
